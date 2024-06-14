731a39f6cebdce9667a6162952a5841fa8b36781
Elias Dobrin <elias@dobrin.net>
2024-06-13 18:17:42 -0700

    working (springs) double pendulum

--- src/simulation.c
+++ src/simulation.c
@@ -29,8 +29,15 @@ struct support {
 	} constraints[3];
 };
 
-#define jcount 2
+#define jcount 3
 struct joint joints[jcount] = {
+	{
+		.mass = {
+			.m = 1.0,
+			.p = {0.0, 0.5, 0.0},
+			.v = {0.0, 0.0, 0.0}
+		}
+	},
 	{
 		.mass = {
 			.m = 1.0,
@@ -41,27 +48,40 @@ struct joint joints[jcount] = {
 	{
 		.mass = {
 			.m = 1.0,
-			.p = {1.0, 0.0, 0.0},
-			.v = {0.0, 0.0, 0.0}
+			.p = {0.0, -0.5, 0.0},
+			.v = {1.0, 0.0, 0.0}
 		}
 	}
 };
 double jforces[jcount][3];
 double jaccelerations[jcount][3];
 
-#define mcount 1
+#define mcount 2
 struct member members[mcount] = {
 	{
 		.spring = {
-			.k = 1.0,
+			.k = 100.0,
 			.m1 = &joints[0].mass,
 			.m2 = &joints[1].mass,
-			.l0 = 0.75
+			.l0 = 0.5
 		},
 		.damper = {
 			.c = 1.0,
 			.m1 = &joints[0].mass,
 			.m2 = &joints[1].mass
+		}
+	},
+	{
+		.spring = {
+			.k = 100.0,
+			.m1 = &joints[1].mass,
+			.m2 = &joints[2].mass,
+			.l0 = 0.5
+		},
+		.damper = {
+			.c = 1.0,
+			.m1 = &joints[1].mass,
+			.m2 = &joints[2].mass
 		}
 	}
 };
@@ -71,6 +91,7 @@ struct support supports[scount] = {};
 
 int iteration;
 double delta_time = 0.01;
+double gravity = 1.0;
 
 void step(void)
 {
@@ -91,6 +112,12 @@ void step(void)
 			jforces[j][c] = 0.0;
 			jaccelerations[j][c] = 0.0;
 		}
+	}
+
+	for(int j = 0; j < jcount; j++)
+	{
+		jaccelerations[j][1] = -gravity;
+		jforces[j][1] = joints[j].mass.m * jaccelerations[j][1];
 	}
 
 	for(int m = 0; m < mcount; m++)
@@ -137,10 +164,22 @@ void step(void)
 			joint->mass.v[c] += 0.5 * jaccelerations[j][c] * delta_time;
 		}
 	}
+
+	if(jcount > 0)
+	{
+		struct joint *joint = &joints[0];
+		for(int c = 0; c < 3; c++)
+		{
+			joint->mass.p[c] = 0.0;
+			joint->mass.v[c] = 0.0;
+		}
+		joint->mass.p[1] = 0.5;
+	}
 }
 
 int picture;
 int width = 1000, height = 1000;
+double center[2] = {0.0, 0.0};
 double zoom = 0.8;
 cairo_surface_t *surface;
 cairo_t *context;
@@ -163,19 +202,37 @@ void draw(void)
 	cairo_translate(context, 0.5 * ((double) width), 0.5 * ((double) height));
 	cairo_scale(context, (double) width, (double) height);
 	cairo_scale(context, zoom, zoom);
-	cairo_translate(context, -0.5, -0.5);
+	cairo_translate(context, center[0], center[1]);
+
+	cairo_set_line_cap(context, CAIRO_LINE_CAP_ROUND);
+	cairo_set_line_join(context, CAIRO_LINE_JOIN_ROUND);
 
 	for(int j = 0; j < jcount; j++)
 	{
 		struct joint *joint = &joints[j];
-		cairo_arc(context, joint->mass.p[0], joint->mass.p[1], 0.03, 0, M_TAU);
+		cairo_arc(context, joint->mass.p[0], joint->mass.p[1], 0.04, 0, M_TAU);
 		cairo_set_source_rgb(context, 1.0, 1.0, 1.0);
 		cairo_fill_preserve(context);
-		cairo_set_line_width(context, 0.003);
+		cairo_set_line_width(context, 0.01);
 		cairo_set_source_rgb(context, 0.0, 0.0, 0.0);
 		cairo_stroke(context);
+		cairo_arc(context, joint->mass.p[0], joint->mass.p[1], 0.02, 0, M_TAU);
+		cairo_set_source_rgb(context, 0.0, 0.0, 0.0);
+		cairo_fill(context);
 	}
 
+	for(int m = 0; m < mcount; m++)
+	{
+		struct member *member = &members[m];
+		cairo_move_to(context, member->spring.m1->p[0], member->spring.m1->p[1]);
+		cairo_line_to(context, member->spring.m2->p[0], member->spring.m2->p[1]);
+		cairo_set_line_width(context, 0.04);
+		cairo_set_source_rgb(context, 0.0, 0.0, 0.0);
+		cairo_stroke_preserve(context);
+		cairo_set_line_width(context, 0.02);
+		cairo_set_source_rgb(context, 1.0, 1.0, 1.0);
+		cairo_stroke(context);
+	}
 
 
 
@@ -195,7 +252,6 @@ void init(void)
 {
 	iteration = 0;
 	picture = 0;
-
 	for(int j = 0; j < jcount; j++)
 	{
 		for(int c = 0; c < 3; c++)
@@ -204,6 +260,11 @@ void init(void)
 			jaccelerations[j][c] = 0.0;
 		}
 	}
+	for(int j = 0; j < jcount; j++)
+	{
+		jaccelerations[j][1] = -gravity;
+		jforces[j][1] = joints[j].mass.m * jaccelerations[j][1];
+	}
 }
 
 int iterations = 1000;
@@ -211,7 +272,6 @@ int pictures = 100;
 
 int main(void)
 {
-	printf("main program started.\n");
 	init();
 	int substeps = iterations / pictures;
 	for(int p = 0; p < pictures; p++)
