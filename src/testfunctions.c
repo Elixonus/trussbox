44d6bdf0304f177a2029e3c5431fda6a6735e84e
Elias Dobrin <elias@dobrin.net>
2024-06-26 19:45:07 -0700

    trying to fix tests.

--- src/testfunctions.c
+++ src/testfunctions.c
@@ -1,18 +1,14 @@
-// find if energy of dampened floating system reaches 0
-// find how closely oscillation matches dampened wave equation
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include "dampspring.h"
 
-#ifndef M_PI
-#define M_PI 3.141592654
-#endif
-#ifndef M_TAU
-#define M_TAU 6.283185307
-#endif
+// find if energy of dampened floating system reaches 0
+// find how closely oscillation matches dampened wave equation
+
+constexpr double pi = 4.0 * atan(1.0);
+constexpr double tau = 2.0 * pi;
 
 double randd()
 {
@@ -24,7 +20,6 @@ int main(void)
 {
 	srand(time(NULL));
 
-	int nt = 1000000;
 	for(int s = 0; s < 100; s++)
 	{
 		double x0 = 10.0 * randd() * pow(10.0, 2.0 * (randd() - 0.5));
@@ -49,24 +44,22 @@ int main(void)
 			s--;
 			continue;
 		}
-		double w = sqrt(w2);
-		double dt = 1e-6 * (0.5 + 0.5 * randd()) * M_TAU / w;
-		double x;
-		double a;
 		double t = 0.0;
+		double dt = 1e-6;
+		int nt = 1000000;
 		for(int i = 0; i < nt; i++)
 		{
-			mass.v[0] += 0.5 * a * dt;
-			mass.p[0] += mass.v[0] * dt;
 			double f = (sforce(&spring) + dforce(&damper)) * mass.p[0] / fabs(mass.p[0]);
-			a = f / mass.m;
-			mass.v[0] += 0.5 * a * dt;
+			double a = f / mass.m;
+			mass.v[0] += a * dt;
+			mass.p[0] += mass.v[0] * dt;
 			t += dt;
-			x = x0 * exp(-0.5 * damper.c / mass.m * t) * cos(w * t);
 			//printf("x=%f, p[0]=%f, f=%f, a=%f, v[0]=%f\n", x, mass.p[0], f, a, mass.v[0]);
 		}
+		double w = sqrt(w2);
+		double x = x0 * exp(-0.5 * damper.c / mass.m * t) * cos(w * t);
 		double error = fabs(mass.p[0] - x) / x0;
-		if(fabs(error) > 1e-2)
+		if(fabs(error) > 1e-2 || true)
 		{
 			fprintf(stderr, "warning: harmonic oscillation test failed\n");
 			fprintf(stderr, "parameters: x0=%lf, m=%lf, k=%lf, c=%lf, dt=%lf, nt=%d\n", x0, mass.m, spring.k, damper.c, dt, nt);
