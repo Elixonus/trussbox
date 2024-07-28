2461bc31b5ac6889c0dfe846bc65924ddb26cb26
Elias Dobrin <elias@dobrin.net>
2024-07-27 20:01:58 -0700

    Update solvesystem.c

--- src/solvesystem.c
+++ src/solvesystem.c
@@ -15,94 +15,74 @@ int main(void)
 		return 1;
 	}
 
-	while (true)
+	struct mass mass;
+	while (scanf("m %lf (%lf %lf %lf) <%lf %lf %lf>\n", &mass.m, &mass.p[0], &mass.p[1], &mass.p[2], &mass.v[0], &mass.v[1], &mass.v[2]) == 7)
 	{
-		struct mass mass;
-		if (scanf("m %lf (%lf %lf %lf) <%lf %lf %lf>\n", &mass.m, &mass.p[0], &mass.p[1], &mass.p[2], &mass.v[0], &mass.v[1], &mass.v[2]) == 7)
+		mcount++;
+		if (mcount > mlimit)
 		{
-			mcount++;
-			if (mcount > mlimit)
-			{
-				int quotient = mlimit;
-				mlimit *= 2;
-				if (mlimit / 2 != quotient)
-				{
-					return 1;
-				}
-
-				masses = realloc(masses, mlimit * sizeof(struct mass));
-				if(masses == nullptr)
-				{
-					return 1;
-				}
-			}
-			masses[mcount - 1] = mass;
-			continue;
-		}
-
-		int index1, index2;
-
-		struct spring spring;
-		if (scanf("s %lf %d %d %lf\n", &spring.k, &index1, &index2, &spring.l0) == 4)
-		{
-			scount++;
-			if (scount > slimit)
-			{
-				int quotient = slimit;
-				slimit *= 2;
-				if (slimit / 2 != quotient)
-				{
-					return 1;
-				}
-
-				springs = realloc(springs, slimit * sizeof(struct spring));
-				if(springs == nullptr)
-				{
-					return 1;
-				}
-			}
-
-			if (index1 >= mcount || index1 < 0 || index2 >= mcount || index2 < 0)
+			int quotient = mlimit;
+			mlimit *= 2;
+			if (mlimit / 2 != quotient)
 			{
 				return 1;
 			}
-			spring.m1 = &masses[index1];
-			spring.m2 = &masses[index2];
-			springs[scount - 1] = spring;
-			continue;
-		}
 
-		struct damper damper;
-		if (scanf("d %lf %d %d\n", &damper.c, &index1, &index2) == 3)
-		{
-			dcount++;
-			if (dcount > dlimit)
-			{
-				int quotient = dlimit;
-				dlimit *= 2;
-				if (dlimit / 2 != quotient)
-				{
-					return 1;
-				}
-
-				dampers = realloc(dampers, dlimit * sizeof(struct damper));
-				if(dampers == nullptr)
-				{
-					return 1;
-				}
-			}
-
-			if (index1 >= mcount || index1 < 0 || index2 >= mcount || index2 < 0)
+			masses = realloc(masses, mlimit * sizeof(struct mass));
+			if(masses == nullptr)
 			{
 				return 1;
 			}
-			damper.m1 = &masses[index1];
-			damper.m2 = &masses[index2];
-			dampers[dcount - 1] = damper;
-			continue;
+		}
+		masses[mcount - 1] = mass;
+	}
+
+	int index1, index2;
+
+	struct spring spring;
+	while (scanf("s %lf %d %d %lf\n", &spring.k, &index1, &index2, &spring.l0) == 4)
+	{
+		scount++;
+		if (scount > slimit)
+		{
+			slimit *= 2;
+			springs = realloc(springs, slimit * sizeof(struct spring));
+			if(springs == nullptr)
+			{
+				return 1;
+			}
 		}
 
-		break;
+		if (index1 >= mcount || index1 < 0 || index2 >= mcount || index2 < 0)
+		{
+			return 1;
+		}
+		spring.m1 = &masses[index1];
+		spring.m2 = &masses[index2];
+		springs[scount - 1] = spring;
+	}
+
+	struct damper damper;
+	while (scanf("d %lf %d %d\n", &damper.c, &index1, &index2) == 3)
+	{
+		dcount++;
+		if (dcount > dlimit)
+		{
+			dlimit *= 2;
+			dampers = realloc(dampers, dlimit * sizeof(struct damper));
+			if(dampers == nullptr)
+			{
+				return 1;
+			}
+		}
+
+		if (index1 >= mcount || index1 < 0 || index2 >= mcount || index2 < 0)
+		{
+			return 1;
+		}
+		damper.m1 = &masses[index1];
+		damper.m2 = &masses[index2];
+		dampers[dcount - 1] = damper;
 	}
 
 	double (*mforces)[3] = malloc(mcount * sizeof(double[3]));
@@ -119,10 +99,6 @@ int main(void)
 		double force = sforce(spring);
 		sforces[s] = force;
 		double length = mdistance(spring->m1, spring->m2);
-		if (fabs(length) < 1e-9)
-		{
-			continue;
-		}
 		double direction[3];
 		for (int c = 0; c < 3; c++)
 		{
@@ -153,10 +129,6 @@ int main(void)
 		double force = dforce(damper);
 		dforces[d] = force;
 		double length = mdistance(damper->m1, damper->m2);
-		if (fabs(length) < 1e-9)
-		{
-			continue;
-		}
 		double direction[3];
 		for (int c = 0; c < 3; c++)
 		{
