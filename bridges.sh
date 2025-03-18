9172693940c7779379a0146118ffb96c582edc7d
Elias Dobrin <elias@dobrin.net>
2025-03-18 11:31:10 -0700

    added parker bridge

--- bridges.sh
+++ bridges.sh
@@ -37,3 +37,15 @@ echo "howe.txt
 1.0
 1.0
 tmp/bridges/howe" | source custom.sh
+echo "parker.txt
+9.8
+3.0
+1000.0
+60.0
+1920
+1080
+0.5
+0.125
+1.0
+1.0
+tmp/bridges/parker" | source custom.sh
--- src/dampspring.c
+++ src/dampspring.c
@@ -1,19 +1,22 @@
 #include <math.h>
 
-struct mass {
+struct mass
+{
 	double m;
 	double p[2];
 	double v[2];
 };
 
-struct spring {
+struct spring
+{
 	double k;
 	struct mass *m1;
 	struct mass *m2;
 	double l0;
 };
 
-struct damper {
+struct damper
+{
 	double c;
 	struct mass *m1;
 	struct mass *m2;
