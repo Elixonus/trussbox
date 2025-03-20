6445c571a0729e4352db7d7cfeb4a8c9a8f2521e
Elias Dobrin <elias@dobrin.net>
2025-03-19 22:06:14 -0700

    added even more error messages

--- src/solvetruss.c
+++ src/solvetruss.c
@@ -312,111 +312,112 @@ int render(void)
 	sprintf(filename, "%s/%09d.png", dirname, frame + 1);
 	if(cairo_surface_write_to_png(surface, filename) != CAIRO_STATUS_SUCCESS)
 	{
-		fprintf(stderr, "error: write: frame filename: \"%s\"\n", filename);
+		fprintf(stderr, "error: write: frame: filename: \"%s\"\n", filename);
 		return 1;
 	}
 	cairo_surface_destroy(surface);
 	frame++;
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
 	if(argc != 10)
 	{
-		fprintf(stderr, "error: count: arguments: %d of 9\n", argc - 1);
+		fprintf(stderr, "error: logic: arguments: %d of 9 provided\n", argc - 1);
 		return 1;
 	}
 	if(sscanf(argv[1], "%1000s", dirname) != 1)
 	{
-		fprintf(stderr, "error: parse: dirname argument: \"%s\" [1]\n", argv[1]);
+		fprintf(stderr, "error: parse: dirname argument: \"%s\" (1)\n", argv[1]);
 		return 1;
 	}
 	if(sscanf(argv[2], "gravity=%lf", &gravity) != 1)
 	{
-		fprintf(stderr, "error: parse: gravity argument: \"%s\" [2]\n", argv[2]);
+		fprintf(stderr, "error: parse: gravity argument: \"%s\" (2)\n", argv[2]);
 		return 1;
 	}
 	if(sscanf(argv[3], "timef=%lf", &timef) != 1)
 	{
-		fprintf(stderr, "error: parse: timef argument: \"%s\" [3]\n", argv[3]);
+		fprintf(stderr, "error: parse: timef argument: \"%s\" (3)\n", argv[3]);
 		return 1;
 	}
 	if(timef < epsilon)
 	{
-		fprintf(stderr, "error: scale: timef variable: %.1e not greater than %.1e\n", timef, epsilon);
+		fprintf(stderr, "error: limit: timef variable: %.1e not greater than %.1e\n", timef, epsilon);
 		return 1;
 	}
 	if(sscanf(argv[4], "srate=%lf", &srate) != 1)
 	{
-		fprintf(stderr, "error: parse: srate argument: \"%s\" [4]\n", argv[4]);
+		fprintf(stderr, "error: parse: srate argument: \"%s\" (4)\n", argv[4]);
 		return 1;
 	}
 	if(srate < epsilon)
 	{
-		fprintf(stderr, "error: scale: srate variable: %.1e not greater than %.1e\n", srate, epsilon);
+		fprintf(stderr, "error: limit: srate variable: %.1e not greater than %.1e\n", srate, epsilon);
 		return 1;
 	}
 	dtime = 1.0 / srate;
 	if(dtime < epsilon)
 	{
-		fprintf(stderr, "error: scale: dtime variable: %.1e not greater than %.1e\n", dtime, epsilon);
+		fprintf(stderr, "error: limit: dtime variable: %.1e not greater than %.1e\n", dtime, epsilon);
 		return 1;
 	}
 	stepf = ((int) round(srate * timef)) - 1;
 	if(stepf < 0)
 	{
-		fprintf(stderr, "error: sign: stepf variable: %d not positive\n", stepf);
+		fprintf(stderr, "error: limit: stepf variable: %d not positive\n", stepf);
 		return 1;
 	}
 	if(sscanf(argv[5], "frate=%lf", &frate) != 1)
 	{
-		fprintf(stderr, "error: parse: frate argument: \"%s\" [5]\n", argv[5]);
+		fprintf(stderr, "error: parse: frate argument: \"%s\" (5)\n", argv[5]);
 		return 1;
 	}
 	if(frate < epsilon)
 	{
-		fprintf(stderr, "error: scale: frate variable: %.1e not greater than %.1e\n", frate, epsilon);
+		fprintf(stderr, "error: limit: frate variable: %.1e not greater than %.1e\n", frate, epsilon);
 		return 1;
 	}
 	framef = ((int) round(frate * timef)) - 1;
 	if(framef < 0)
 	{
-		fprintf(stderr, "error: sign: framef variable: %d not positive\n", framef);
+		fprintf(stderr, "error: limit: framef variable: %d not positive\n", framef);
 		return 1;
 	}
 	if(sscanf(argv[6], "fsize=%dx%d", &fsize[0], &fsize[1]) != 2)
 	{
-		fprintf(stderr, "error: parse: fsize argument: \"%s\" [6]\n", argv[6]);
+		fprintf(stderr, "error: parse: fsize argument: \"%s\" (6)\n", argv[6]);
 		return 1;
 	}
 	if(fsize[0] < 64 || fsize[1] < 64)
 	{
-		fprintf(stderr, "error: scale: fsize variable: %dx%d not larger than or equivalent to 64x64\n", fsize[0], fsize[1]);
+		fprintf(stderr, "error: limit: fsize variable: %dx%d not larger than or equivalent to 64x64\n", fsize[0], fsize[1]);
 		return 1;
 	}
 	if(sscanf(argv[7], "fcenter=(%lf %lf)", &fcenter[0], &fcenter[1]) != 2)
 	{
-		fprintf(stderr, "error: parse: fcenter argument: \"%s\" [7]\n", argv[7]);
+		fprintf(stderr, "error: parse: fcenter argument: \"%s\" (7)\n", argv[7]);
 		return 1;
 	}
 	if(sscanf(argv[8], "fzoom=%lf", &fzoom) != 1)
 	{
-		fprintf(stderr, "error: parse: fzoom argument: \"%s\" [8]\n", argv[8]);
+		fprintf(stderr, "error: parse: fzoom argument: \"%s\" (8)\n", argv[8]);
 		return 1;
 	}
 	if(fzoom < epsilon)
 	{
-		fprintf(stderr, "error: scale: fzoom variable: %.1e not greater than %.1e\n", fzoom, epsilon);
+		fprintf(stderr, "error: limit: fzoom variable: %.1e not greater than %.1e\n", fzoom, epsilon);
 		return 1;
 	}
 	if(sscanf(argv[9], "fscale=%lf", &fscale) != 1)
 	{
-		fprintf(stderr, "error: parse: fscale argument: \"%s\" [9]\n", argv[9]);
+		fprintf(stderr, "error: parse: fscale argument: \"%s\" (9)\n", argv[9]);
 		return 1;
 	}
 	if(fscale < epsilon)
 	{
-		fprintf(stderr, "error: scale: fscale variable: %.1e not greater than %.1e\n", fscale, epsilon);
+		fprintf(stderr, "error: limit: fscale variable: %.1e not greater than %.1e\n", fscale, epsilon);
 		return 1;
 	}
 	if(scanf("joints=%d\n", &jcount) != 1)
@@ -426,7 +427,7 @@ int main(int argc, char **argv)
 	}
 	if(jcount < 0)
 	{
-		fprintf(stderr, "error: count: joints: %d not positive or zero\n", jcount);
+		fprintf(stderr, "error: logic: joints parameter: %d not positive or zero\n", jcount);
 		return 1;
 	}
 	joints = malloc(jcount * sizeof(struct joint));
@@ -453,19 +454,27 @@ int main(int argc, char **argv)
 		if(scanf(
 			"mass=%lf position=(%lf %lf) velocity=<%lf %lf>\n",
 			&joint.mass.m, &joint.mass.p[0], &joint.mass.p[1], &joint.mass.v[0], &joint.mass.v[1]
-		) != 5) return 1;
-		if(joint.mass.m < epsilon) return 1;
+		) != 5)
+		{
+			fprintf(stderr, "error: parse: joint%d variable\n", j + 1);
+			return 1;
+		}
+		if(joint.mass.m < epsilon)
+		{
+			fprintf(stderr, "error: limit: joint%d variable: mass parameter: %.1e not greater than %.1e\n", j + 1, joint.mass.m, epsilon);
+			return 1;
+		}
 		joints[j] = joint;
 		jaccelerations[j] = malloc(2 * sizeof(double));
 		if(jaccelerations[j] == nullptr)
 		{
-			fprintf(stderr, "error: memory: jaccelerations[%d] variable: %zuB allocation\n", j + 1, 2 * sizeof(double));
+			fprintf(stderr, "error: memory: jacceleration%d variable: %zuB allocation\n", j + 1, 2 * sizeof(double));
 			return 1;
 		}
 		jforces[j] = malloc(2 * sizeof(double));
 		if(jforces[j] == nullptr)
 		{
-			fprintf(stderr, "error: memory: jforces[%d] variable: %zuB allocation\n", j + 1, 2 * sizeof(double));
+			fprintf(stderr, "error: memory: jforce%d variable: %zuB allocation\n", j + 1, 2 * sizeof(double));
 			return 1;
 		}
 		for(int c = 0; c < 2; c++)
@@ -481,7 +490,7 @@ int main(int argc, char **argv)
 	}
 	if(mcount < 0)
 	{
-		fprintf(stderr, "error: count: members: %d not positive or zero\n", mcount);
+		fprintf(stderr, "error: logic: members parameter: %d not positive or zero\n", mcount);
 		return 1;
 	}
 	members = malloc(mcount * sizeof(struct member));
@@ -521,9 +530,22 @@ int main(int argc, char **argv)
 		if(scanf(
 			"joint1=%d joint2=%d stiffness=%lf length0=%lf dampening=%lf\n",
 			&jindex1, &jindex2, &member.spring.k, &member.spring.l0, &member.damper.c
-		) != 5) return 1;
+		) != 5)
+		{
+			fprintf(stderr, "error: parse: member%d variable\n", m + 1);
+			return 1;
+		}
 		jindex1--, jindex2--;
-		if(jindex1 < 0 || jindex1 >= jcount || jindex2 < 0 || jindex2 >= jcount) return 1;
+		if(jindex1 < 0 || jindex1 >= jcount)
+		{
+			fprintf(stderr, "error: logic: member%d variable: joint1 parameter: %d does not exist\n", m + 1, jindex1 + 1);
+			return 1;
+		}
+		if(jindex2 < 0 || jindex2 >= jcount)
+		{
+			fprintf(stderr, "error: logic: member%d variable: joint2 parameter: %d does not exist\n", m + 1, jindex2 + 1);
+			return 1;
+		}
 		for(int m2 = 0; m2 < m; m2++)
 			if(
 				(
@@ -534,12 +556,20 @@ int main(int argc, char **argv)
 					members[m2].spring.m1 == &joints[jindex2].mass &&
 					members[m2].spring.m2 == &joints[jindex1].mass
 				)
-			) return 1;
+			)
+			{
+				fprintf(stderr, "error: logic: member%d variable: joint1 and joint2 parameters: (%d and %d) or (%d and %d) already used\n", m + 1, jindex1 + 1, jindex2 + 1, jindex2 + 1, jindex1 + 1);
+				return 1;
+			}
 		member.spring.m1 = &joints[jindex1].mass;
 		member.spring.m2 = &joints[jindex2].mass;
 		member.damper.m1 = &joints[jindex1].mass;
 		member.damper.m2 = &joints[jindex2].mass;
-		if(member.spring.l0 < epsilon) return 1;
+		if(member.spring.l0 < epsilon)
+		{
+			fprintf(stderr, "error: limit: member%d variable: length0 parameter: %.1e not greater than %.1e\n", m + 1, member.spring.l0, epsilon);
+			return 1;
+		}
 		members[m] = member;
 		mlengths[m] = 0.0;
 		mdisplacements[m] = 0.0;
@@ -553,7 +583,7 @@ int main(int argc, char **argv)
 	}
 	if(scount < 0)
 	{
-		fprintf(stderr, "error: count: supports: %d not positive or zero\n", scount);
+		fprintf(stderr, "error: logic: supports parameter: %d not positive or zero\n", scount);
 		return 1;
 	}
 	supports = malloc(scount * sizeof(struct support));
@@ -573,10 +603,22 @@ int main(int argc, char **argv)
 		int jindex;
 		char axes[3];
 		struct support support;
-		if(scanf("joint=%d axes=%2s\n", &jindex, axes) != 2) return 1;
+		if(scanf("joint=%d axes=%2s\n", &jindex, axes) != 2)
+		{
+			fprintf(stderr, "error: parse: support%d variable\n", s + 1);
+			return 1;
+		}
 		jindex--;
-		if(jindex < 0 || jindex >= jcount) return 1;
-		for(int s2 = 0; s2 < s; s2++) if(supports[s2].constraint.m == &joints[jindex].mass) return 1;
+		if(jindex < 0 || jindex >= jcount)
+		{
+			fprintf(stderr, "error: logic: support%d variable: joint parameter: %d does not exist\n", s + 1, jindex + 1);
+			return 1;
+		}
+		for(int s2 = 0; s2 < s; s2++) if(supports[s2].constraint.m == &joints[jindex].mass)
+		{
+			fprintf(stderr, "error: logic: support%d variable: joint parameter: %d already used\n", s + 1, jindex + 1);
+			return 1;
+		}
 		support.constraint.m = &joints[jindex].mass;
 		if(strcmp(axes, "xy") == 0 || strcmp(axes, "yx") == 0)
 		{
@@ -593,14 +635,18 @@ int main(int argc, char **argv)
 			support.constraint.a[0] = false;
 			support.constraint.a[1] = true;
 		}
-		else return 1;
+		else
+		{
+			fprintf(stderr, "error: parse: support%d variable: axes parameter: \"%s...\" not available\n", s + 1, axes);
+			return 1;
+		}
 		for(int c = 0; c < 2; c++)
 			support.constraint.p[c] = joints[jindex].mass.p[c];
 		supports[s] = support;
 		sreactions[s] = malloc(2 * sizeof(double));
 		if(sreactions[s] == nullptr)
 		{
-			fprintf(stderr, "error: memory: sreactions[%d] variable: %zuB allocation\n", s + 1, 2 * sizeof(double));
+			fprintf(stderr, "error: memory: sreaction%d variable: %zuB allocation\n", s + 1, 2 * sizeof(double));
 			return 1;
 		}
 		for(int c = 0; c < 2; c++) sreactions[s][c] = 0.0;
@@ -612,7 +658,7 @@ int main(int argc, char **argv)
 	}
 	if(lcount < 0)
 	{
-		fprintf(stderr, "error: count: loads: %d not positive or zero\n", lcount);
+		fprintf(stderr, "error: logic: loads parameter: %d not positive or zero\n", lcount);
 		return 1;
 	}
 	loads = malloc(lcount * sizeof(struct load));
@@ -628,9 +674,17 @@ int main(int argc, char **argv)
 		if(scanf(
 			"joint=%d force=<%lf %lf>\n",
 			&jindex, &load.action.f[0], &load.action.f[1]
-		) != 3) return 1;
+		) != 3)
+		{
+			fprintf(stderr, "error: parse: load%d variable\n", l + 1);
+			return 1;
+		}
 		jindex--;
-		if(jindex < 0 || jindex >= jcount) return 1;
+		if(jindex < 0 || jindex >= jcount)
+		{
+			fprintf(stderr, "error: logic: load%d variable: joint parameter: %d does not exist\n", l + 1, jindex + 1);
+			return 1;
+		}
 		load.action.m = &joints[jindex].mass;
 		loads[l] = load;
 	}
---
+++ test.sh
@@ -0,0 +1,1 @@
+./bin/solvetruss test/frames gravity=9.8 timef=1.0 srate=10000.0 frate=60.0 fsize=1920x1080 "fcenter=(0.5 0.125)" fzoom=1.0 fscale=1.0 < warren.txt
