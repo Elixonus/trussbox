f8fe252846790f567efc1359507a1e2c1be87a73
Elias Dobrin <elias@dobrin.net>
2025-03-24 16:45:54 -0700

    added feedback program: problem(t) + solution(t+dt) -> problem(t+dt)

--- src/feedback.c
+++ src/feedback.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 #include "dampspring.h"
 
@@ -35,171 +34,21 @@ struct load
 };
 
 struct joint *joints;
-double **jaccelerations;
-double **jforces;
 int jcount;
 
 struct member *members;
-double *mlengths;
-double *mdisplacements;
-double *mvelocities;
-double *mforces;
 int mcount;
 
 struct support *supports;
-double **sreactions;
 int scount;
 
 struct load *loads;
 int lcount;
 
-double gravity;
-double dtime;
-double time;
-double timef;
-int step;
-int stepf;
-double srate;
-
 double epsilon = 1.0e-18;
 
-void solve(void)
+int main(void)
 {
-	for(int j = 0; j < jcount; j++)
-	{
-		struct joint *joint = &joints[j];
-		for(int c = 0; c < 2; c++)
-		{
-			joint->mass.v[c] += 0.5 * jaccelerations[j][c] * dtime;
-			joint->mass.p[c] += joint->mass.v[c] * dtime;
-		}
-	}
-	for(int j = 0; j < jcount; j++)
-	{
-		struct joint *joint = &joints[j];
-		for(int c = 0; c < 2; c++)
-			jforces[j][c] = 0.0;
-		jforces[j][1] = -gravity * joint->mass.m;
-	}
-	for(int l = 0; l < lcount; l++)
-	{
-		struct load *load = &loads[l];
-		int jindex;
-		for(int j = 0; j < jcount; j++) if(&joints[j].mass == load->action.m)
-			jindex = j;
-		for(int c = 0; c < 2; c++)
-			jforces[jindex][c] += load->action.f[c];
-	}
-	for(int m = 0; m < mcount; m++)
-	{
-		struct member *member = &members[m];
-		double length = mdistance(member->spring.m1, member->spring.m2);
-		if(length < epsilon) continue;
-		double force = sforce(&member->spring) + dforce(&member->damper);
-		double direction[2];
-		int jindex1, jindex2;
-		for(int j = 0; j < jcount; j++)
-		{
-			if(&joints[j].mass == member->spring.m1)
-				jindex1 = j;
-			if(&joints[j].mass == member->spring.m2)
-				jindex2 = j;
-		}
-		for(int c = 0; c < 2; c++)
-		{
-			direction[c] = (joints[jindex2].mass.p[c] - joints[jindex1].mass.p[c]) / length;
-			jforces[jindex1][c] -= direction[c] * force;
-			jforces[jindex2][c] += direction[c] * force;
-		}
-		mforces[m] = force;
-		mlengths[m] = length;
-		mdisplacements[m] = sdisplacement(&member->spring);
-		mvelocities[m] = dvelocity(&member->damper);
-	}
-	for(int j = 0; j < jcount; j++)
-	{
-		struct joint *joint = &joints[j];
-		for(int c = 0; c < 2; c++)
-		{
-			jaccelerations[j][c] = jforces[j][c] / (joint->mass.m > epsilon ? joint->mass.m : epsilon);
-		}
-	}
-	for(int j = 0; j < jcount; j++)
-	{
-		struct joint *joint = &joints[j];
-		for(int c = 0; c < 2; c++)
-		{
-			joint->mass.v[c] += 0.5 * jaccelerations[j][c] * dtime;
-		}
-	}
-	for(int s = 0; s < scount; s++)
-	{
-		struct support *support = &supports[s];
-		int jindex;
-		for(int j = 0; j < jcount; j++) if(&joints[j].mass == support->constraint.m)
-			jindex = j;
-		for(int c = 0; c < 2; c++)
-		{
-			if(support->constraint.a[c])
-			{
-				support->constraint.m->p[c] = support->constraint.p[c];
-				support->constraint.m->v[c] = 0.0;
-				sreactions[s][c] = -jforces[jindex][c];
-			}
-		}
-	}
-	time += dtime;
-	step++;
-}
-
-int main(int argc, char **argv)
-{
-	if(argc != 4)
-	{
-		fprintf(stderr, "error: count: arguments: %d of 3 provided\n", argc - 1);
-		fprintf(stderr, "usage: arguments: %s gravity=float timef=float srate=float\n", argv[0]);
-		return 1;
-	}
-	if(sscanf(argv[1], "gravity=%lf", &gravity) != 1)
-	{
-		fprintf(stderr, "error: parse: gravity argument: %s (1)\n", argv[2]);
-		fprintf(stderr, "usage: gravity argument: gravity=float (1)\n");
-		return 1;
-	}
-	if(sscanf(argv[2], "timef=%lf", &timef) != 1)
-	{
-		fprintf(stderr, "error: parse: timef argument: %s (2)\n", argv[3]);
-		fprintf(stderr, "usage: timef argument: timef=float (2)\n");
-		return 1;
-	}
-	if(timef < epsilon)
-	{
-		fprintf(stderr, "error: limit: timef argument: %.1e not greater than %.1e\n", timef, epsilon);
-		return 1;
-	}
-	if(sscanf(argv[3], "srate=%lf", &srate) != 1)
-	{
-		fprintf(stderr, "error: parse: srate argument: %s (3)\n", argv[4]);
-		fprintf(stderr, "usage: srate argument: srate=float (3)\n");
-		return 1;
-	}
-	if(srate < epsilon)
-	{
-		fprintf(stderr, "error: limit: srate argument: %.1e not greater than %.1e\n", srate, epsilon);
-		return 1;
-	}
-	dtime = 1.0 / srate;
-	if(dtime < epsilon)
-	{
-		fprintf(stderr, "error: limit: dtime variable: %.1e not greater than %.1e\n", dtime, epsilon);
-		return 1;
-	}
-	stepf = ((int) round(srate * timef)) - 1;
-	if(stepf < 0)
-	{
-		fprintf(stderr, "error: limit: stepf variable: %d not positive\n", stepf);
-		return 1;
-	}
 	if(scanf("joints=%d\n", &jcount) != 1)
 	{
 		fprintf(stderr, "error: parse: joints parameter\n");
@@ -215,18 +64,6 @@ int main(int argc, char **argv)
 	if(!joints)
 	{
 		fprintf(stderr, "error: create: joints array: %zdB allocation\n", jcount * sizeof(struct joint));
-		return 1;
-	}
-	jaccelerations = malloc(jcount * sizeof(double *));
-	if(!jaccelerations)
-	{
-		fprintf(stderr, "error: create: jaccelerations array: %zdB allocation\n", jcount * sizeof(double *));
-		return 1;
-	}
-	jforces = malloc(jcount * sizeof(double *));
-	if(!jforces)
-	{
-		fprintf(stderr, "error: create: jforces array: %zdB allocation\n", jcount * sizeof(double *));
 		return 1;
 	}
 	for(int j = 0; j < jcount; j++)
@@ -247,23 +84,6 @@ int main(int argc, char **argv)
 			return 1;
 		}
 		joints[j] = joint;
-		jaccelerations[j] = malloc(2 * sizeof(double));
-		if(!jaccelerations[j])
-		{
-			fprintf(stderr, "error: create: jacceleration%d array: %zdB allocation\n", j + 1, 2 * sizeof(double));
-			return 1;
-		}
-		jforces[j] = malloc(2 * sizeof(double));
-		if(!jforces[j])
-		{
-			fprintf(stderr, "error: create: jforce%d array: %zdB allocation\n", j + 1, 2 * sizeof(double));
-			return 1;
-		}
-		for(int c = 0; c < 2; c++)
-		{
-			jaccelerations[j][c] = 0.0;
-			jforces[j][c] = 0.0;
-		}
 	}
 	if(scanf("members=%d\n", &mcount) != 1)
 	{
@@ -280,30 +100,6 @@ int main(int argc, char **argv)
 	if(!members)
 	{
 		fprintf(stderr, "error: create: members array: %zdB allocation\n", mcount * sizeof(struct member));
-		return 1;
-	}
-	mlengths = malloc(mcount * sizeof(double));
-	if(!mlengths)
-	{
-		fprintf(stderr, "error: create: mlengths array: %zdB allocation\n", mcount * sizeof(double));
-		return 1;
-	}
-	mdisplacements = malloc(mcount * sizeof(double));
-	if(!mdisplacements)
-	{
-		fprintf(stderr, "error: create: mdisplacements array: %zdB allocation\n", mcount * sizeof(double));
-		return 1;
-	}
-	mvelocities = malloc(mcount * sizeof(double));
-	if(!mvelocities)
-	{
-		fprintf(stderr, "error: create: mvelocities array: %zdB allocation\n", mcount * sizeof(double));
-		return 1;
-	}
-	mforces = malloc(mcount * sizeof(double));
-	if(!mforces)
-	{
-		fprintf(stderr, "error: create: mforces array: %zdB allocation\n", mcount * sizeof(double));
 		return 1;
 	}
 	for(int m = 0; m < mcount; m++)
@@ -355,10 +151,6 @@ int main(int argc, char **argv)
 			return 1;
 		}
 		members[m] = member;
-		mlengths[m] = 0.0;
-		mdisplacements[m] = 0.0;
-		mvelocities[m] = 0.0;
-		mforces[m] = 0.0;
 	}
 	if(scanf("supports=%d\n", &scount) != 1)
 	{
@@ -375,12 +167,6 @@ int main(int argc, char **argv)
 	if(!supports)
 	{
 		fprintf(stderr, "error: create: supports array: %zdB allocation\n", scount * sizeof(struct support));
-		return 1;
-	}
-	sreactions = malloc(scount * sizeof(double *));
-	if(!sreactions)
-	{
-		fprintf(stderr, "error: create: sreactions array: %zdB allocation\n", scount * sizeof(double *));
 		return 1;
 	}
 	for(int s = 0; s < scount; s++)
@@ -430,13 +216,6 @@ int main(int argc, char **argv)
 		for(int c = 0; c < 2; c++)
 			support.constraint.p[c] = joints[jindex].mass.p[c];
 		supports[s] = support;
-		sreactions[s] = malloc(2 * sizeof(double));
-		if(!sreactions[s])
-		{
-			fprintf(stderr, "error: create: sreaction%d array: %zdB allocation\n", s + 1, 2 * sizeof(double));
-			return 1;
-		}
-		for(int c = 0; c < 2; c++) sreactions[s][c] = 0.0;
 	}
 	if(scanf("loads=%d\n", &lcount) != 1)
 	{
@@ -477,60 +256,85 @@ int main(int argc, char **argv)
 		load.action.m = &joints[jindex].mass;
 		loads[l] = load;
 	}
-	time = 0.0;
-	step = 0;
-	while(step <= stepf)
-		solve();
-/*
-	frame = 0;
-	while(frame <= framef)
+	int jcount2;
+	if(scanf("joints=%d\n", &jcount2) != 1)
 	{
-		if(render() != 0) return 1;
-		while(step * (framef + 1) < frame * (stepf + 1))
+		fprintf(stderr, "error: parse: joints parameter (solution)\n");
+		fprintf(stderr, "usage: joints parameter (solution): joints=float\n");
+		return 1;
+	}
+	if(jcount2 != jcount)
+	{
+		fprintf(stderr, "error: clash: joints parameters (problem and solution): %d and %d\n", jcount, jcount2);
+		return 1;
+	}
+	for(int j = 0; j < jcount; j++)
+	{
+		struct joint *joint = &joints[j];
+		if(scanf(
+			"force=<%*f %*f> position=(%lf %lf) velocity=<%lf %lf>\n",
+			&joint->mass.p[0], &joint->mass.p[1], &joint->mass.v[0], &joint->mass.v[1]
+		) != 4)
 		{
-			if(solve() != 0) return 1;
+			fprintf(stderr, "error: parse: joint%d line (solution)\n", j + 1);
+			fprintf(stderr, "usage: joint line (solution): mass=float position=(float float) velocity=(float float)\n");
+			return 1;
 		}
 	}
-*/
 	printf("joints=%d\n", jcount);
 	for(int j = 0; j < jcount; j++)
 	{
 		struct joint *joint = &joints[j];
 		printf(
-			"force=<%.9e %.9e> position=(%.9e %.9e) velocity=<%.9e %.9e>\n",
-			jforces[j][0], jforces[j][1], joint->mass.p[0], joint->mass.p[1], joint->mass.v[0], joint->mass.v[1]
+			"mass=%.9e position=(%.9e %.9e) velocity=<%.9e %.9e>\n",
+			joint->mass.m, joint->mass.p[0], joint->mass.p[1], joint->mass.v[0], joint->mass.v[1]
 		);
 	}
 	printf("members=%d\n", mcount);
 	for(int m = 0; m < mcount; m++)
 	{
+		struct member *member = &members[m];
+		int jindex1, jindex2;
+		for(int j = 0; j < jcount; j++)
+		{
+			if(&joints[j].mass == member->spring.m1)
+				jindex1 = j;
+			if(&joints[j].mass == member->spring.m2)
+				jindex2 = j;
+		}
 		printf(
-			"force=%.9e displacement=%.9e length=%.9e velocity=%.9e\n",
-			mforces[m], mdisplacements[m], mlengths[m], mvelocities[m]
+			"joint1=%d joint2=%d stiffness=%.9e length0=%.9e dampening=%.9e\n",
+			jindex1 + 1, jindex2 + 1, member->spring.k, member->spring.l0, member->damper.c
 		);
 	}
 	printf("supports=%d\n", scount);
 	for(int s = 0; s < scount; s++)
 	{
-		printf("reaction=<%.9e %.9e>\n", sreactions[s][0], sreactions[s][1]);
+		struct support *support = &supports[s];
+		int jindex;
+		for(int j = 0; j < jcount; j++) if(&joints[j].mass == support->constraint.m)
+			jindex = j;
+		char axes[101];
+		if(support->constraint.a[0] && support->constraint.a[1])
+			sprintf(axes, "xy");
+		if(support->constraint.a[0] && !support->constraint.a[1])
+			sprintf(axes, "x");
+		if(!support->constraint.a[0] && support->constraint.a[1])
+			sprintf(axes, "y");
+		printf("joint=%d axes=%s\n", jindex + 1, axes);
+	}
+	printf("loads=%d\n", lcount);
+	for(int l = 0; l < lcount; l++)
+	{
+		struct load *load = &loads[l];
+		int jindex;
+		for(int j = 0; j < jcount; j++) if(&joints[j].mass == load->action.m)
+			jindex = j;
+		printf("joint=%d force=<%.9e %.9e>\n", jindex + 1, load->action.f[0], load->action.f[1]);
 	}
 	free(joints);
-	for(int j = 0; j < jcount; j++)
-	{
-		free(jaccelerations[j]);
-		free(jforces[j]);
-	}
-	free(jaccelerations);
-	free(jforces);
 	free(members);
-	free(mlengths);
-	free(mdisplacements);
-	free(mvelocities);
-	free(mforces);
 	free(supports);
-	for(int s = 0; s < scount; s++)
-		free(sreactions[s]);
-	free(sreactions);
 	free(loads);
 	return 0;
 }
