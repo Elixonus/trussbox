6d77d05bd91f138bc5aa9e055c6df1941a9fc392
Elias Dobrin <elias@dobrin.net>
2025-04-06 14:07:00 -0700

    added standard input check and stderr print to trussutils

--- src/trussutils.c
+++ src/trussutils.c
@@ -62,4 +62,107 @@ int main(int argc, char **argv)
 		fprintf(stderr, "error: create: joints array: %zdB allocation\n", jcount * sizeof(struct joint));
 		return 1;
 	}
+	for(int j = 0; j < jcount; j++)
+	{
+		struct joint joint;
+		if(scanf(
+			"mass=%lf position=(%lf %lf) velocity=<%lf %lf>\n",
+			&joint.mass.m, &joint.mass.p[0], &joint.mass.p[1], &joint.mass.v[0], &joint.mass.v[1]
+		) != 5)
+		{
+			fprintf(stderr, "error: parse: joint line (%d)\n", j + 1);
+			fprintf(stderr, "usage: joint line: mass=float position=(float float) velocity=(float float)\n");
+			return 1;
+		}
+		if(joint.mass.m < epsilon)
+		{
+			fprintf(stderr, "error: limit: joint line (%d): mass parameter: %.1e not greater than %.1e\n", j + 1, joint.mass.m, epsilon);
+			return 1;
+		}
+		joints[j] = joint;
+	}
+	if(scanf("members=%d\n", &mcount) != 1)
+	{
+		fprintf(stderr, "error: parse: members parameter\n");
+		fprintf(stderr, "usage: members parameter: members=float\n");
+		return 1;
+	}
+	if(mcount < 0)
+	{
+		fprintf(stderr, "error: count: members parameter: %d not positive nor zero\n", mcount);
+		return 1;
+	}
+	members = malloc(mcount * sizeof(struct member));
+	if(!members)
+	{
+		fprintf(stderr, "error: create: members array: %zdB allocation\n", mcount * sizeof(struct member));
+		return 1;
+	}
+	for(int m = 0; m < mcount; m++)
+	{
+		int jindex1, jindex2;
+		struct member member;
+		if(scanf(
+			"joint1=%d joint2=%d stiffness=%lf length0=%lf dampening=%lf\n",
+			&jindex1, &jindex2, &member.spring.k, &member.spring.l0, &member.damper.c
+		) != 5)
+		{
+			fprintf(stderr, "error: parse: member line (%d)\n", m + 1);
+			fprintf(stderr, "usage: member line: joint1=index joint2=index stiffness=float length0=float dampening=float\n");
+			return 1;
+		}
+		jindex1--, jindex2--;
+		if(jindex1 < 0 || jindex1 >= jcount)
+		{
+			fprintf(stderr, "error: index: member line (%d): joint1 parameter: %d does not exist\n", m + 1, jindex1 + 1);
+			return 1;
+		}
+		if(jindex2 < 0 || jindex2 >= jcount)
+		{
+			fprintf(stderr, "error: index: member line (%d): joint2 parameter: %d does not exist\n", m + 1, jindex2 + 1);
+			return 1;
+		}
+		for(int m2 = 0; m2 < m; m2++)
+			if(
+				(
+					members[m2].spring.m1 == &joints[jindex1].mass &&
+					members[m2].spring.m2 == &joints[jindex2].mass
+				) ||
+				(
+					members[m2].spring.m1 == &joints[jindex2].mass &&
+					members[m2].spring.m2 == &joints[jindex1].mass
+				)
+				)
+			{
+				fprintf(stderr, "error: index: member line (%d): joint1 and joint2 parameters: (%d and %d) or (%d and %d) already in use\n", m + 1, jindex1 + 1, jindex2 + 1, jindex2 + 1, jindex1 + 1);
+				return 1;
+			}
+		member.spring.m1 = &joints[jindex1].mass;
+		member.spring.m2 = &joints[jindex2].mass;
+		member.damper.m1 = &joints[jindex1].mass;
+		member.damper.m2 = &joints[jindex2].mass;
+		if(member.spring.l0 < epsilon)
+		{
+			fprintf(stderr, "error: limit: member line (%d): length0 parameter: %.1e not greater than %.1e\n", m + 1, member.spring.l0, epsilon);
+			return 1;
+		}
+		members[m] = member;
+	}
+	if(scanf("supports=%d\n", &scount) != 1)
+	{
+		fprintf(stderr, "error: parse: supports parameter\n");
+		fprintf(stderr, "usage: supports parameter: supports=float\n");
+		return 1;
+	}
+	if(scount < 0)
+	{
+		fprintf(stderr, "error: count: supports parameter: %d not positive nor zero\n", scount);
+		return 1;
+	}
+	supports = malloc(scount * sizeof(struct support));
+	if(!supports)
+	{
+		fprintf(stderr, "error: create: supports array: %zdB allocation\n", scount * sizeof(struct support));
+		return 1;
+	}
 }
